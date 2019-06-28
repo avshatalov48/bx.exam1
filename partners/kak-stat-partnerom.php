30127390fc24c2008c12be329db070fe6c1fcb66
Alexander Shatalov <alexander.shataloff@gmail.com>
2019-06-28 09:48:09 +0300

    [ex1-2] Метаданные статичных страниц: раздел партнерам.

--- partners/kak-stat-partnerom.php
+++ partners/kak-stat-partnerom.php
@@ -1,5 +1,7 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("keywords", "стать партнером, лидер отрасли");
+$APPLICATION->SetPageProperty("description", "Партнерам. Как стать партнером лидера отрасли");
 $APPLICATION->SetTitle("Как стать партнером");
 ?>
 
