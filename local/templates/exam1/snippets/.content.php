574aa605bb41c76c5192b6969455dd2b93418cbd
Alexander Shatalov <alexander.shataloff@gmail.com>
2019-07-03 12:36:25 +0300

    [ex1-12] Ускорение работы с контентом: html заготовка с контента inner.html (Сниппеты).

---
+++ local/templates/exam1/snippets/.content.php
@@ -0,0 +1,5 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+$SNIPPETS = [];
+$SNIPPETS['snippet0001.snp'] = ['title' => 'inner.html', 'description' => 'Заготовка с контента inner.html'];
