b51ead399803735a6b65a66aa6daf2d4ff57f351
Alexander Shatalov <alexander.shataloff@gmail.com>
2019-07-03 10:32:16 +0300

    [ex1-7] Удаляю лишние файлы. Интеграция шаблона цепочки навигации.

--- local/templates/exam1/components/bitrix/breadcrumb/inner/template.php
+++ local/templates/exam1/components/bitrix/breadcrumb/inner/template.php
@@ -4,7 +4,6 @@ if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 /**
  * @global CMain $APPLICATION
  */
-
 global $APPLICATION;
 
 //delayed function must return a string
@@ -13,43 +12,23 @@ if(empty($arResult))
 
 $strReturn = '';
 
-//we can't use $APPLICATION->SetAdditionalCSS() here because we are inside the buffered function GetNavChain()
-$css = $APPLICATION->GetCSSArray();
-if(!is_array($css) || !in_array("/bitrix/css/main/font-awesome.css", $css))
-{
-	$strReturn .= '<link href="'.CUtil::GetAdditionalFileURL("/bitrix/css/main/font-awesome.css").'" type="text/css" rel="stylesheet" />'."\n";
-}
-
-$strReturn .= '<div class="bx-breadcrumb" itemprop="http://schema.org/breadcrumb" itemscope itemtype="http://schema.org/BreadcrumbList">';
+$strReturn .= '<div class="breadcrumbs-box" itemprop="http://schema.org/breadcrumb" itemscope itemtype="http://schema.org/BreadcrumbList"><div class="inner-wrap">';
 
 $itemSize = count($arResult);
 for($index = 0; $index < $itemSize; $index++)
 {
 	$title = htmlspecialcharsex($arResult[$index]["TITLE"]);
-	$arrow = ($index > 0? '<i class="fa fa-angle-right"></i>' : '');
 
 	if($arResult[$index]["LINK"] <> "" && $index != $itemSize-1)
 	{
-		$strReturn .= '
-			<div class="bx-breadcrumb-item" id="bx_breadcrumb_'.$index.'" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
-				'.$arrow.'
-				<a href="'.$arResult[$index]["LINK"].'" title="'.$title.'" itemprop="url">
-					<span itemprop="name">'.$title.'</span>
-				</a>
-				<meta itemprop="position" content="'.($index + 1).'" />
-			</div>';
+        $strReturn .= '<a href="' . $arResult[$index]["LINK"] . '" title="' . $title . '" itemprop="url">' . $title . '</a>';
 	}
 	else
 	{
-		$strReturn .= '
-			<div class="bx-breadcrumb-item" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
-				'.$arrow.'
-				<span itemprop="name">'.$title.'</span>
-				<meta itemprop="position" content="'.($index + 1).'" />
-			</div>';
+        $strReturn .= '<span>' . $title . '</span>';
 	}
 }
 
-$strReturn .= '<div style="clear:both"></div></div>';
+$strReturn .= '</div></div>';
 
 return $strReturn;
