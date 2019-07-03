a51e15bda9db45862672b4d4fd31081852c099f7
Alexander Shatalov <alexander.shataloff@gmail.com>
2019-07-03 17:23:11 +0300

    [ex1-15] Интеграция слайдера отзыва. Обрезка текста анонса. Уменьшение фото. Удаление лишних файлов компонента.

--- local/templates/exam1/components/bitrix/news.list/slider/template.php
+++ local/templates/exam1/components/bitrix/news.list/slider/template.php
@@ -1,4 +1,6 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+    die();
+}
 /** @var array $arParams */
 /** @var array $arResult */
 /** @global CMain $APPLICATION */
@@ -12,75 +14,80 @@
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 ?>
-<div class="news-list">
-<?if($arParams["DISPLAY_TOP_PAGER"]):?>
-	<?=$arResult["NAV_STRING"]?><br />
-<?endif;?>
-<?foreach($arResult["ITEMS"] as $arItem):?>
-	<?
-	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-	$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-	?>
-	<p class="news-item" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
-		<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
-			<?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
-				<a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><img
-						class="preview_picture"
-						border="0"
-						src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
-						width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
-						height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
-						alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
-						title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
-						style="float:left"
-						/></a>
-			<?else:?>
-				<img
-					class="preview_picture"
-					border="0"
-					src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
-					width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
-					height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
-					alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
-					title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
-					style="float:left"
-					/>
-			<?endif;?>
-		<?endif?>
-		<?if($arParams["DISPLAY_DATE"]!="N" && $arItem["DISPLAY_ACTIVE_FROM"]):?>
-			<span class="news-date-time"><?echo $arItem["DISPLAY_ACTIVE_FROM"]?></span>
-		<?endif?>
-		<?if($arParams["DISPLAY_NAME"]!="N" && $arItem["NAME"]):?>
-			<?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
-				<a href="<?echo $arItem["DETAIL_PAGE_URL"]?>"><b><?echo $arItem["NAME"]?></b></a><br />
-			<?else:?>
-				<b><?echo $arItem["NAME"]?></b><br />
-			<?endif;?>
-		<?endif;?>
-		<?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arItem["PREVIEW_TEXT"]):?>
-			<?echo $arItem["PREVIEW_TEXT"];?>
-		<?endif;?>
-		<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
-			<div style="clear:both"></div>
-		<?endif?>
-		<?foreach($arItem["FIELDS"] as $code=>$value):?>
-			<small>
-			<?=GetMessage("IBLOCK_FIELD_".$code)?>:&nbsp;<?=$value;?>
-			</small><br />
-		<?endforeach;?>
-		<?foreach($arItem["DISPLAY_PROPERTIES"] as $pid=>$arProperty):?>
-			<small>
-			<?=$arProperty["NAME"]?>:&nbsp;
-			<?if(is_array($arProperty["DISPLAY_VALUE"])):?>
-				<?=implode("&nbsp;/&nbsp;", $arProperty["DISPLAY_VALUE"]);?>
-			<?else:?>
-				<?=$arProperty["DISPLAY_VALUE"];?>
-			<?endif?>
-			</small><br />
-		<?endforeach;?>
-	</p>
-<?endforeach;?>
-<?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
-	<br /><?=$arResult["NAV_STRING"]?>
-<?endif;?>
+<div class="item-wrap">
+    <div class="rew-footer-carousel">
+
+        <? if ($arParams["DISPLAY_TOP_PAGER"]): ?>
+            <?= $arResult["NAV_STRING"] ?><br/>
+        <? endif; ?>
+
+        <? foreach ($arResult["ITEMS"] as $arItem): ?>
+            <?
+            $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'],
+                CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+            $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'],
+                CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"),
+                ["CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')]);
+            ?>
+
+            <div class="item">
+                <div class="side-block side-opin" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
+                    <div class="inner-block">
+                        <div class="title">
+                            <div class="photo-block">
+                                <? if ($arItem["PREVIEW_PICTURE"]): ?>
+                                    <?php
+                                    $sPreviewPicture = $arItem["PREVIEW_PICTURE"];
+                                    $file = CFile::ResizeImageGet($sPreviewPicture, ['width' => 39, 'height' => 39],
+                                        BX_RESIZE_IMAGE_PROPORTIONAL, true);
+                                    $sPictureSrc = $file['src'];
+                                    ?>
+                                <? else: ?>
+                                    <?php
+                                    // При отсутствии изображения должна выводиться картинка – заглушка,
+                                    // файл no_photo_left_block.jpg из материалов.
+                                    $sPictureSrc = SITE_TEMPLATE_PATH . '/img/rew/no_photo_left_block.jpg';
+                                    ?>
+                                <? endif; ?>
+                                <img src="<?= $sPictureSrc ?>" alt="">
+                            </div>
+
+                            <div class="name-block">
+                                <a href="<?= $arItem["DETAIL_PAGE_URL"] ?>">
+                                    <? if ($arParams["DISPLAY_NAME"] != "N" && $arItem["NAME"]): ?>
+                                        <?= $arItem["NAME"] ?>
+                                    <? endif; ?>
+                                </a>
+                            </div>
+
+                            <div class="pos-block">
+                                <? //<Должность> ?>
+                                <? if ($arItem["DISPLAY_PROPERTIES"]["POSITION"]["DISPLAY_VALUE"]): ?>
+                                    <?= ucfirst($arItem["DISPLAY_PROPERTIES"]["POSITION"]["DISPLAY_VALUE"]); ?>,
+                                <? endif ?>
+                                <? //<Название компании> ?>
+                                <? if ($arItem["DISPLAY_PROPERTIES"]["COMPANY"]["DISPLAY_VALUE"]): ?>
+                                    <?= $arItem["DISPLAY_PROPERTIES"]["COMPANY"]["DISPLAY_VALUE"]; ?>
+                                <? endif ?>
+                            </div>
+                        </div>
+
+                        <? //<Показывать первые 150 символов текста отзыва из анонса> ?>
+                        <? if ($arParams["DISPLAY_PREVIEW_TEXT"] != "N" && $arItem["PREVIEW_TEXT"]): ?>
+                            <div class="text-block">
+                                <?= substr($arItem["PREVIEW_TEXT"], 0, 150); ?>
+                                <? if (strlen($arItem["PREVIEW_TEXT"]) > 150): ?>
+                                    ...
+                                <? endif; ?>
+                            </div>
+                        <? endif; ?>
+                    </div>
+                </div>
+            </div>
+        <? endforeach; ?>
+
+        <? if ($arParams["DISPLAY_BOTTOM_PAGER"]): ?>
+            <br/><?= $arResult["NAV_STRING"] ?>
+        <? endif; ?>
+    </div>
 </div>
