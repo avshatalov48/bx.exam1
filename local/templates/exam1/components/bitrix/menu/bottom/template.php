10e511d19b4dacfddcff12171f5ae8a7db9d5133
Alexander Shatalov <alexander.shataloff@gmail.com>
2019-07-03 10:47:24 +0300

    [ex1-8] Удаление лишних файлов в шаблоне. Добавление пунктов в нижнее меню. Интеграция.

--- local/templates/exam1/components/bitrix/menu/bottom/template.php
+++ local/templates/exam1/components/bitrix/menu/bottom/template.php
@@ -1,7 +1,7 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 
 <?if (!empty($arResult)):?>
-<ul class="left-menu">
+<ul>
 
 <?
 foreach($arResult as $arItem):
--- local/templates/exam1/footer.php
+++ local/templates/exam1/footer.php
@@ -118,14 +118,22 @@ use Bitrix\Main\Localization\Loc;
         <nav class="main-menu">
             <div class="item">
                 <div class="title-block">О магазине</div>
-                <ul>
-                    <li><a href="">Отзывы</a>
-                    </li>
-                    <li><a href="">Руководство </a>
-                    </li>
-                    <li><a href="">История</a>
-                    </li>
-                </ul>
+                <? $APPLICATION->IncludeComponent("bitrix:menu", "bottom", Array(
+                    "ALLOW_MULTI_SELECT" => "N",    // Разрешить несколько активных пунктов одновременно
+                    "CHILD_MENU_TYPE" => "left",    // Тип меню для остальных уровней
+                    "DELAY" => "N",    // Откладывать выполнение шаблона меню
+                    "MAX_LEVEL" => "1",    // Уровень вложенности меню
+                    "MENU_CACHE_GET_VARS" => array(    // Значимые переменные запроса
+                        0 => "",
+                    ),
+                    "MENU_CACHE_TIME" => "3600",    // Время кеширования (сек.)
+                    "MENU_CACHE_TYPE" => "A",    // Тип кеширования
+                    "MENU_CACHE_USE_GROUPS" => "Y",    // Учитывать права доступа
+                    "ROOT_MENU_TYPE" => "bottom",    // Тип меню для первого уровня
+                    "USE_EXT" => "Y",    // Подключать файлы с именами вида .тип_меню.menu_ext.php
+                ),
+                    false
+                ); ?>
             </div>
             <div class="item">
                 <div class="title-block">Каталог товаров</div>
