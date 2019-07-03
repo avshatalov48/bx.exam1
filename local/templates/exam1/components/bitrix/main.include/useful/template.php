2feecda80cfb0143b892be3e769efa522f41ce9b
Alexander Shatalov <alexander.shataloff@gmail.com>
2019-07-03 12:15:30 +0300

    [ex1-11] Интеграция. Текст областей.

--- local/templates/exam1/components/bitrix/main.include/useful/template.php
+++ local/templates/exam1/components/bitrix/main.include/useful/template.php
@@ -13,5 +13,13 @@ if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 
-if($arResult["FILE"] <> '')
-	include($arResult["FILE"]);
+if (!empty($arResult["FILE"]) && strlen(trim(file_get_contents($arResult["FILE"]))) > 0): ?>
+    <div class="side-block side-anonse">
+        <div class="title-block"><span class="i i-title01"></span>Полезная информация!</div>
+        <div class="item">
+            <p>
+                <? include($arResult["FILE"]); ?>
+            </p>
+        </div>
+    </div>
+<? endif; ?>
